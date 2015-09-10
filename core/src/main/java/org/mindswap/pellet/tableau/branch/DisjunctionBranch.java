df01109b2382f6e88d1ba45af313577cdc56f363
Mokarrom <mokarrombd@gmail.com>
2015-09-10 11:56:47 -0300

    Minor change in log file.

--- core/src/main/java/org/mindswap/pellet/tableau/branch/DisjunctionBranch.java
+++ core/src/main/java/org/mindswap/pellet/tableau/branch/DisjunctionBranch.java
@@ -80,7 +80,7 @@ public class DisjunctionBranch extends Branch {
     
     protected String getDebugMsg() {
         return "Rank # "+myRank+" : DISJ: Branch (" + getBranch() + ") try (" + (getTryNext() + 1) + "/" + getTryCount()
-            + ") " + node + " " +  ATermUtils.toString( disj[getTryNext()] ) + " " + ATermUtils.toString(disjunction);
+            + ") " + node + " " +  ATermUtils.toString( disj[getTryNext()] ) + " OF " + ATermUtils.toString(disjunction);
     }
 	
 	public DisjunctionBranch copyTo(ABox abox) {
